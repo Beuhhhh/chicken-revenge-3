7e0cccd2d7ad5252824f00e10c7c98a5d019a91a
Beuhhhh <mantere0901@gmail.com>
2023-02-16 10:42:22 +0200

    pushed  to git
    pushed  to git

---
+++ Assets/bullet_movement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class bullet_movement : MonoBehaviour
+
+{
+    private Vector3 dir;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+
+
+
+    public void shootBullet(string direction)
+    {
+        Transform sprite = transform.GetChild(0);
+        if (direction == "up")
+        {
+            dir = Vector3.up;
+            // transform.rotation = Quaternion.Euler(0, 0, 90);
+            sprite.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        if (direction == "down")
+        {
+            dir = Vector3.down;
+            sprite.transform.rotation = Quaternion.Euler(0, 0, 270);
+
+
+        }
+        if (direction == "left")
+        {
+            dir = Vector3.left;
+            sprite.transform.rotation = Quaternion.Euler(0, 0, 180);
+
+
+        }
+        if (direction == "right")
+        {
+            dir = Vector3.right;
+            sprite.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(dir * Time.deltaTime * 10);
+        if (Time.time - startTime > 5)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/chicken_movement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class chicken_movement : MonoBehaviour
+
+{
+    [SerializeField]
+    public GameObject up;
+
+    [SerializeField]
+    public GameObject down;
+
+    [SerializeField]
+    public GameObject side;
+
+    [SerializeField]
+    public GameObject bullet;
+
+
+
+
+    public string direction = "up";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            newBullet.GetComponent<bullet_movement>().shootBullet(direction);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction = "up";
+            transform.Translate(Vector3.up * Time.deltaTime);
+            up.SetActive(true);
+            down.SetActive(false);
+            side.SetActive(false);
+            transform.localScale = new Vector3(4, 4, 4);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction = "down";
+            transform.Translate(Vector3.down * Time.deltaTime);
+            up.SetActive(false);
+            side.SetActive(false);
+            down.SetActive(true);
+            transform.localScale = new Vector3(4, 4, 4);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = "left";
+            transform.Translate(Vector3.left * Time.deltaTime);
+            up.SetActive(false);
+            down.SetActive(false);
+            side.SetActive(true);
+            transform.localScale = new Vector3(4, 4, 4);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = "right";
+
+            transform.Translate(Vector3.right * Time.deltaTime);
+            up.SetActive(false);
+            down.SetActive(false);
+            side.SetActive(true);
+            transform.localScale = new Vector3(-4, 4, 4);
+        }
+
+    }
+
+
+}
