0f536d0f999ce9020858c6c8af55037de2b34b42
Beuhhhh <mantere0901@gmail.com>
2023-02-16 11:09:34 +0200

    fox movement

--- Assets/fox_ai_movement.cs
+++ Assets/fox_ai_movement.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class fox_ai_movement : MonoBehaviour
+
 {
+    private GameObject target_chicken;
     // Start is called before the first frame update
     void Start()
     {
 
+
     }
+
 
     // Update is called once per frame
     void Update()
+
     {
-        // fox moves to player
-
-
-
+        if (target_chicken == null)
+        {
+            target_chicken = GameObject.Find("player chicken");
+        }
+        // fox moves towards player chicken to make fox run away from player -0.01f
+        transform.position = Vector3.MoveTowards(transform.position, target_chicken.transform.position, 0.01f);
     }
 }
