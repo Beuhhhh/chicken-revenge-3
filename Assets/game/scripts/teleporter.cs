aacbfddd312725f8b183cbe5371810c62337f70c
Beuhhhh <mantere0901@gmail.com>
2023-03-01 14:42:27 +0200

    shop, teleports, blood, fixes

---
+++ Assets/game/scripts/teleporter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleporter : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        //if player collides for 2 seconds start level game_horde
+        if (col.gameObject.tag == "player")
+        {
+            Application.LoadLevel("game_horde");
+            col.gameObject.transform.position = transform.position;
+            transform.Rotate(new Vector3(0, 0, 90 * Time.deltaTime));
+        }
+
+    }
+
+}
