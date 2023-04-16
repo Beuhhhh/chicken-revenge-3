cd6f9d6bb3a934aac7e27555938f17838b8891e8
Klicer <51763508+Klicer@users.noreply.github.com>
2023-04-16 17:00:49 +0300

    Fix fox damage

--- Assets/game/scripts/fox_health.cs
+++ Assets/game/scripts/fox_health.cs
@@ -4,47 +4,31 @@ using UnityEngine;
 
 public class fox_health : MonoBehaviour
 {
-
     public int health = 3;
     public bool bullet_hit_fox = false;
     public GameObject blood_s;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-
-    }
     // Update is called once per frame
     void Update()
-
     {
-
         // if fox health is 0, fox is destroyed and spawns blood
         if (health <= 0)
         {
             Destroy(gameObject);
             Instantiate(blood_s, transform.position, Quaternion.identity);
-
-
-
         }
         if (bullet_hit_fox == true)
         {
             health -= 1;
-
         }
+    }
 
-        void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "bullet")
         {
-            if (col.gameObject.tag == "bullet")
-            {
-                bullet_hit_fox = true;
-                Destroy(col.gameObject);
-            }
-
-
+            bullet_hit_fox = true;
+            Destroy(col.gameObject);
         }
     }
 }
